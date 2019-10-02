3cfe19d47ad16cc4b901a0b997d0b8be29433ac8
Geovanny Buitrago <geovanny91_1@hotmail.com>
2019-10-01 22:49:42 -0500

    Creating MainFragment with a couple of menu options, no using navigation component since it's just one fragment in a simple app.

--- app/src/main/java/com/gyros/mvi_arch/MainActivity.kt
+++ app/src/main/java/com/gyros/mvi_arch/MainActivity.kt
@@ -8,5 +8,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        showMainFragment()
+    }
+
+    /**
+     * This is because is just one fragment, besides it's a simple app
+     * normally I'd do this with navigation component, which is more powerfull
+     */
+    fun showMainFragment(){
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container,MainFragment(),"Main Fragment")
     }
 }
